e85be7a1c5641bedcba074abf51ab43113d9c092
Yehudit Ilson <yehuditi@one1.co.il>
2017-12-25 03:12:32 +0200

    save

--- ls75/Node-ls-01/hello.js
+++ ls75/Node-ls-01/hello.js
@@ -6,7 +6,7 @@ var url = require('url');
 var callbackModule = require('./callback');
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    console.log(callbackModule.sayHello());
+    console.log('gunibush >>>>' + callbackModule.sayHello());
 
     // the Query string
     var queryString = url.parse(req.url).query;
@@ -21,3 +21,28 @@ http.createServer(function(req, res) {
     });
 
 }).listen(1330);
+// var evenDouble = function(num, callback) {
+//     var timeWait = Math.random();
+//     if (num % 2 == 0) {
+//         setTimeout(function() {
+//             callback(null, num*2, timeWait);
+//         }, timeWait * 1000)
+//     } else {
+//         setTimeout(function() {
+//             callback(new Error('not even'));
+//         }, timeWait * 1000)
+//     }
+// }
+// var getCourseStudentData;
+// function testCompletion() {
+//       if (courses.coursesRetrieved.status && students.studentsRetrieved.status) {
+//             if (entity === "course"){
+//                   courseSave.displayAfterSave(serverResponse, action);
+//             } 
+//             else {
+//                   studentSave.displayAfterSave(serverResponse, action);
+//             } 
+//             clearInterval(getCourseStudentData);
+//       }
+// }
+// getCourseStudentData = setInterval(testCompletion, 500);
